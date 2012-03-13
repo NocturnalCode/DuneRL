ced348a20adf4dc148583ced7088dfd8ff4a68d0
Tom <tom@nocturnalcode.com>
2012-03-13 16:12:08 +1300

    Shadow function

--- Code/LightFilterShadow.cpp
+++ Code/LightFilterShadow.cpp
@@ -17,44 +17,88 @@
 #include "Point.h"
 #include "Tile.h"
 
+inline double abs(double val)
+{
+    return val > 0 ? val : -val;
+}
+
+double weighting(Point direction, int i, int j)
+{
+    if (i ==0 && j==0) {
+        return 0;
+    }
+    int hits = 0;
+    if (direction.X + i == 0) {
+        hits++;
+        if (direction.Y == 0) {
+            hits+=2;
+        }
+    }
+    if (direction.Y + j == 0) {
+        hits++;
+        if (direction.X == 0) {
+            hits+=2;
+        }
+    }
+    if (direction.X - i == 0) {
+        hits--;
+        if (direction.Y == 0) {
+            hits-=2;
+        }
+    }
+    if (direction.Y - j == 0) {
+        hits--;
+        if (direction.X == 0) {
+            hits-=2;
+        }
+    }
+    
+    return hits;
+}
 
 Ascii* LightFilterShadow::apply(Lightmap* map, WorldCoord worldPoint, Ascii* ascii)
 {
     //we want to change the ascii now...
     
+    Point direction = Point(1, 1);
     
     Tile* currentTile = map->tileAtPoint(worldPoint);
     WorldCoord coord;
-    coord.X = worldPoint.X - 2;
-    coord.Y = worldPoint.Y - 2;
+    double darken = 0.025;
     
-    for (int i = 0; i<3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = -1; i<2; i++) {
+        for (int j = -1; j < 2; j++) {
+            coord.X = worldPoint.X - i;
+            coord.Y = worldPoint.Y - j;
+            Tile* comparisonTile = map->tileAtPoint(coord);
+            
+//            Tile* less = NULL;
+//            Tile* more = NULL;
+            //direction tells us which is more important
+            double weight = weighting(direction, i, j);
+            if (weight == 0) {
+                continue;
+            }
+            
+            if (weight < 0) {
+                weight = abs(weight);
+                if (comparisonTile->height > currentTile->height) {
+                    for (int z = 0; z<weight; z++) {
+                        ascii->Background.darken(darken);
+                        ascii->Foreground.darken(darken);
+                    }
+                }
+            }
+            else {
+                if (comparisonTile->height < currentTile->height) {
+                    for (int z = 0; z<weight; z++) {
+                        ascii->Background.darken(darken);
+                        ascii->Foreground.darken(darken);
+                    }
+                }
+            }
             
         }
-    }
-    
-    Tile* comparisonTile = map->tileAtPoint(coord);
-    if (comparisonTile->height > currentTile->height) {
-        //ok, we know we have to darken it, but its not clear by how much
-        ascii->Background.darken(0.05);
-        ascii->Foreground.darken(0.05);
-    }
-    coord.X = worldPoint.X - 2;
-    coord.Y = worldPoint.Y - 1;
-    comparisonTile = map->tileAtPoint(coord);
-    if (comparisonTile->height > currentTile->height) {
-        //ok, we know we have to darken it, but its not clear by how much
-        ascii->Background.darken(0.05);
-        ascii->Foreground.darken(0.05);
-    }
-    coord.X = worldPoint.X - 1;
-    coord.Y = worldPoint.Y - 2;
-    comparisonTile = map->tileAtPoint(coord);
-    if (comparisonTile->height > currentTile->height) {
-        //ok, we know we have to darken it, but its not clear by how much
-        ascii->Background.darken(0.05);
-        ascii->Foreground.darken(0.05);
     }
     return ascii;
 }
