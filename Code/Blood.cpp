33fdc83f21e3b86eb22ac2ad289d233bd8652790
Daniel Brooker <dan@nocturnalcode.com>
2012-03-16 12:39:03 +1300

    Objects for eating

---
+++ Code/Blood.cpp
@@ -0,0 +1,14 @@
+//
+//  Blood.cpp
+//  Roguelike
+//
+//  Created by Daniel Brooker on 16/03/12.
+//  Copyright (c) 2012 Nocturnal Code Limited. All rights reserved.
+//
+
+#include "Blood.h"
+
+Blood::Blood() : Object()
+{
+    
+}
---
+++ Code/Blood.h
@@ -0,0 +1,20 @@
+//
+//  Blood.h
+//  Roguelike
+//
+//  Created by Daniel Brooker on 16/03/12.
+//  Copyright (c) 2012 Nocturnal Code Limited. All rights reserved.
+//
+
+#ifndef BLOOD_INCLUDE_H
+#define BLOOD_INCLUDE_H
+
+#include "Object.h"
+
+class Blood : public Object
+{
+public:    
+    Blood();
+};
+
+#endif /* BLOOD_INCLUDE_H */
